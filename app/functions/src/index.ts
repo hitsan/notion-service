ae0f431c6fed523fc9ac5313616c2c0fe539305d
hitsan <soledewa2828@gmail.com>
2023-04-14 01:51:41 +0900

    Add region

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -5,17 +5,24 @@ import {addPageToRoutine} from "./routine";
 
 const jst = "Asia/Tokyo";
 
-export const helloWorld = functions.https.onRequest(
-  async (request, response) => {
-    const date = formatInTimeZone(new Date(), jst, "yyyy-MM-dd");
-    const title = formatInTimeZone(new Date(), jst, "yyyy/MM/dd");
-    addPageToLifelog(title, date);
-    addPageToRoutine(title, date);
+const dairyTask = async () => {
+  const date = formatInTimeZone(new Date(), jst, "yyyy-MM-dd");
+  const title = formatInTimeZone(new Date(), jst, "yyyy/MM/dd");
+  addPageToLifelog(title, date);
+  addPageToRoutine(title, date);
+};
+
+export const notionDairy = functions.region("asia-northeast1").https.onRequest(
+  (request, response) => {
+    dairyTask();
     response.send("Run dairy task");
   });
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule("0 0 * * *")
+exports.scheduledFunctionCrontab = functions
+  .region("asia-northeast1").pubsub
+  .schedule("0 0 * * *")
   .timeZone(jst)
   .onRun(() => {
+    dairyTask();
     functions.logger.info("Run dairy task", {structuredData: true});
   });
