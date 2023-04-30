8b71f23cd9fdab4f055a7fb7fe55281310404b11
hitsan <soledewa2828@gmail.com>
2023-04-30 19:28:17 +0900

    aaa

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import {updateBooksInfo} from "./book-info";
-// import {dairyTask} from "./dairy-task";
+import {dairyTask} from "./dairy-task";
 
 export const addBookInfo = functions.region("asia-northeast1").https.onRequest(
   async (request, response) => {
@@ -12,16 +12,16 @@ export const addBookInfo = functions.region("asia-northeast1").https.onRequest(
     }
   });
 
-// const jst = "Asia/Tokyo";
-// exports.scheduledFunctionCrontab = functions
-//   .region("asia-northeast1").pubsub
-//   .schedule("0 6 * * *")
-//   .timeZone(jst)
-//   .onRun(async () => {
-//     try {
-//       await dairyTask(jst);
-//       functions.logger.info("Succese dairy task", {structuredData: true});
-//     } catch (error: unknown) {
-//       functions.logger.error("Failed dairyTask", {structuredData: true});
-//     }
-//   });
+const jst = "Asia/Tokyo";
+exports.scheduledFunctionCrontab = functions
+  .region("asia-northeast1").pubsub
+  .schedule("0 6 * * *")
+  .timeZone(jst)
+  .onRun(async () => {
+    try {
+      await dairyTask(jst);
+      functions.logger.info("Succese dairy task", {structuredData: true});
+    } catch (error: unknown) {
+      functions.logger.error("Failed dairyTask", {structuredData: true});
+    }
+  });
