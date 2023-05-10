ae9d4f5b5e4f452a748864d47b594a86a338646f
hitsan <soledewa2828@gmail.com>
2023-05-11 01:40:07 +0900

    Add a test

---
+++ app/functions/src/restraunt.ts
@@ -0,0 +1,7 @@
+// import * as functions from "firebase-functions";
+// import {Client} from "@notionhq/client";
+// import axios from "axios";
+
+export const featchShishaPlaceId = async () => {
+  return true;
+};
