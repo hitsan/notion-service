0d8c9cee0612ea5e09de9b702ac4c2a0ef24d6c2
hitsan <soledewa2828@gmail.com>
2023-05-28 18:21:01 +0900

    Force pull

--- app/functions/test/unit/book-info.spec.ts
+++ app/functions/test/unit/book-info.spec.ts
@@ -1,7 +1,9 @@
-import {updateBooksInfo} from "../../src/book-info";
+import {updateBooksInfo, featchBookInfo} from "../../src/watchList/book-info";
 // import {mockedData} from "./book-info-test-data"
 import {Client} from "@notionhq/client";
+import axios from "axios";
 
+jest.mock("axios");
 describe("Update Book Info Test", () => {
   const notionToken = process.env.NOTION_TOKEN || "";
   const notion = new Client({auth: notionToken});
@@ -9,7 +11,40 @@ describe("Update Book Info Test", () => {
 
   const watchListDBId = process.env.NOTION_WATCHLIST_DATABASE_ID || "";
 
-  test("Get weather info", async () => {
+  test("Get Book info", async () => {
+    const mockedData = {
+      data: {
+        items: [
+          {
+            volumeInfo: {
+              authors: ["JK"],
+              title: "ハリー・ポッターと秘密の部屋",
+              publishedDate: "2000/12/12",
+              industryIdentifiers: [
+                {
+                  "type": "ISBN_10",
+                  "identifier": "4915512398"
+                },
+                {
+                  "type": "ISBN_13",
+                  "identifier": "9784915512391"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValueOnce(mockedData);
+
+    const title = "ハリー・ポッターと秘密の部屋";
+    const result = await featchBookInfo(title);
+    expect(result.authors).toEqual("JK");
+    expect(result.title).toEqual("ハリー・ポッターと秘密の部屋");
+    expect(result.publishedDate).toEqual("2000/12/12");
+  });
+
+  test("Update book info", async () => {
     // (notion.databases.query as jest.MockedFunction<typeof notion.databases.query>).mockResolvedValueOnce(mockedData);
     const result = await updateBooksInfo(notion, watchListDBId);
     expect(result).toBeTruthy();
