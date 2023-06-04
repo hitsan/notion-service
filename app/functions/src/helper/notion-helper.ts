57fb7ab6295948eaefb58153ea4bf3401eaef3ce
hitsan <soledewa2828@gmail.com>
2023-06-04 17:56:24 +0900

    Add JSDoc

--- app/functions/src/helper/notion-helper.ts
+++ app/functions/src/helper/notion-helper.ts
@@ -1,11 +1,14 @@
 import {Client} from "@notionhq/client";
-import {TargeBook} from "../service/watchList/book-info"
+import {TargeBook} from "../service/watchList/book-info";
 
 interface SearchdQuery {
   database_id: string,
   filter: any,
 }
 
+/**
+ * Notion Helper
+ */
 export class NotionHelper {
   private static notion: Client = (()=>{
     const notionToken = process.env.NOTION_TOKEN;
@@ -13,20 +16,21 @@ export class NotionHelper {
     return new Client({auth: notionToken});
   })();
 
-  static async featchDbBookContents(dbId: string, query: Object): Promise<TargeBook[]> {
+  /**
+  * Get book contents
+  * @param {string} dbId ID of DB
+  * @param {object} query Filter
+  */
+  static async featchDbBookContents(dbId: string, query: object): Promise<TargeBook[]> {
     const filteringQuery: SearchdQuery = {database_id: dbId, filter: query};
-    try {
-      const response = await this.notion.databases.query(filteringQuery);
-      const bookList = response.results.map((result) => {
-        if (!("properties" in result && "title" in result.properties.Title)) {
-          throw new Error("Ilegal data");
-        }
-        const title = result.properties.Title.title[0].plain_text;
-        return {id: result.id, title: title};
-      });
-      return bookList;
-    } catch (err) {
-      throw err;
-    }
+    const response = await this.notion.databases.query(filteringQuery);
+    const bookList = response.results.map((result) => {
+      if (!("properties" in result && "title" in result.properties.Title)) {
+        throw new Error("Ilegal data");
+      }
+      const title = result.properties.Title.title[0].plain_text;
+      return {id: result.id, title: title};
+    });
+    return bookList;
   }
 }
