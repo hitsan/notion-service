6985f0344a28eef45ebc959e7dea768eb821e28e
hitsan <soledewa2828@gmail.com>
2023-06-13 03:40:53 +0900

    add function that clear update test db

--- app/functions/test/setup-tests.ts
+++ app/functions/test/setup-tests.ts
@@ -1,3 +1,30 @@
 import * as dotenv from "dotenv";
+import {Client} from "@notionhq/client";
 
 dotenv.config({ path: ".env.local" });
+
+const notionToken = process.env.NOTION_TOKEN || "";
+const notion: Client = new Client({auth: notionToken});
+
+// Clear Notion test page
+const clearUpdatePages = () => {
+  const updatePage = process.env.TEST_UPDATE_PAGE || "";
+  const properties = {
+    Name: {
+      title: [
+        {
+          text: {
+            content: "",
+          },
+        },
+      ],
+    },
+    Tags: {
+      multi_select: [],
+    },
+  };
+  const query = {page_id: updatePage, properties: properties};
+  notion.pages.update(query);
+}
+
+clearUpdatePages();
